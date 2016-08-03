d79d0e6385f73f88fc6f7099b5581116609f042b
aftian@localhost <>
2016-08-04 05:58:45 +0700

    48.0-1blankon1

--- debian/config/branch.mk
+++
@@ -1,6 +0,0 @@
-CHANNEL			= beta
-MOZ_WANT_UNIT_TESTS	= 1
-MOZ_ENABLE_BREAKPAD 	= 1
-
-MOZILLA_REPO = http://hg.mozilla.org/releases/mozilla-beta
-L10N_REPO = http://hg.mozilla.org/releases/l10n/mozilla-beta
